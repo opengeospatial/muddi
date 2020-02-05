9331acfee1655310c03579cd4ed484ce7b138e77
LieberJosh <jlieberman@ogc.org>
2020-02-05 15:23:41 -0500

    minor charter updates

--- charter/make_er.sh
+++ charter/make_er.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 asciidoctor  $1.adoc
 asciidoctor-pdf   -a pdf-stylesdir=resources -a pdf-style=ogc -a pdf-fontsdir=resources/fonts $1.adoc
-#asciidoctor  -a numbered -b docbook -d book -o 1$.docbook 1$.adoc
-# pandoc -s 1$.docbook -f docbook -t rtf -s -o 1$.rtf
+asciidoctor  -a numbered -b docbook -d book -o $1.docbook $1.adoc
+pandoc -s $1.docbook -f docbook -t rtf -s -o $1.rtf
